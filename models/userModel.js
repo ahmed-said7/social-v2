f6c574a37ec9842670292cd7e247fa1c49f700e5
Ahmed Said <engahmedsaid125@gmail.com>
2023-10-21 01:06:17 +0300

    1st commit

--- models/userModel.js
+++ models/userModel.js
@@ -6,6 +6,7 @@ const userSchema = new mongoose.Schema(
         password:{type:String,required:true,min:6},
         username:{type:String,required:true},
         email:{type:String,required:true,unique:true},
+        coins:{type:Number,default:0},
         birth:Date,
         cover:String,
         profile:String,
